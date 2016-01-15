c293c73b6b65cab6de29f98ce82735e7e8c5cb95
Codizer <adrianortizmartinez@gmail.com>
2016-01-15 03:16:58 -0600

    Comentario
    Comentario en el método sumar

--- src/Operaciones.java
+++ src/Operaciones.java
@@ -5,6 +5,13 @@
  */
 public class Operaciones {
 
+	/**
+	 * Suma dos enteros y devuelve su valor
+	 * 
+	 * @param n1
+	 * @param n2
+	 * @return
+	 */
 	public int sumar(int n1, int n2) {
 		return n1 + n2;
 	}
