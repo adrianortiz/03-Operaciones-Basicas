c4afab41ab79f63c3c2c05d077305b455bb7c0e2
Codizer <adrianortizmartinez@gmail.com>
2016-01-15 02:59:03 -0600

    Create project
    Create project “03-Operaciones-Basicas”

---
+++ src/AppOperaciones.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Adrian
+ *
+ */
+public class AppOperaciones {
+	
+	/**
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Ventana vtn = new Ventana();
+		vtn.setVisible(true);
+	}
+}
---
+++ src/Operaciones.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ * @author Adrian
+ *
+ */
+public class Operaciones {
+
+	public int sumar(int n1, int n2) {
+		return n1 + n2;
+	}
+}
